114e67c25809c7d80f613f86dfb14f75f290304d
Abdul Hakeem <cb013168@students.apiit.lk>
2024-03-16 12:29:13 +0530

    login page

--- Js/app.js
+++ Js/app.js
@@ -1,33 +1,24 @@
 document.addEventListener('alpine:init', () => {
-    Alpine.store('log',{
-        
-        user:null,
-        users:[
-            {
-                username:'admin',
-                password:'admin',
-                roal:'admin',
-                email:'admin@apiit.com',
-            },
-
-        
-            {
-                username:'user',
-                password:'user',
-                roal:'sys_user',
-                email:'user@apiit.com',
-            }
-
-            
-        ],
-        Authenticate(username,password,email) {
-            let user = this.users.find(function(user) {
-                return user.username === username && user.password === password && user.email === email;
-               
-            });
-
+    Alpine.store('log', {
+      user: null,
+      users: [{
+          username: 'admin',
+          password: 'admin',
+          role: 'admin',
+          email: 'admin@apiit.com',
+        },
+        {
+          username: 'abdul',
+          password: 'Abdul',
+          role: 'admin',
+          email: 'abdul@apiit.com',
         }
+      ],
+      authenticate(username, password, email) {
+        let user = this.users.find(function (user) {
+          return user.username === username && user.password === password && user.email === email;
+        });
+        return user;
+      }
     });
-});
-
-
+  });
