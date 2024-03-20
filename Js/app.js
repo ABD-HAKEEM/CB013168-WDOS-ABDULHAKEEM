56edd1bf021b1360ce271ba07616a8ea930bc3ff
Abdul Hakeem <cb013168@students.apiit.lk>
2024-03-20 09:10:09 +0530

    login page

--- Js/app.js
+++ Js/app.js
@@ -1,24 +1,41 @@
 document.addEventListener('alpine:init', () => {
-    Alpine.store('log', {
+  Alpine.store('log', {
       user: null,
-      users: [{
-          username: 'admin',
-          password: 'admin',
-          role: 'admin',
-          email: 'admin@apiit.com',
-        },
-        {
-          username: 'abdul',
-          password: 'Abdul',
-          role: 'admin',
-          email: 'abdul@apiit.com',
-        }
+      users: [
+          {
+              username: 'admin',
+              password: 'admin',
+              role: 'admin',
+              email: 'admin@apiit.com',
+          },
+          {
+              username: 'abdul',
+              password: 'Abdul',
+              role: 'admin',
+              email: 'abdul@apiit.com',
+          }
       ],
-      authenticate(username, password, email) {
-        let user = this.users.find(function (user) {
-          return user.username === username && user.password === password && user.email === email;
-        });
-        return user;
+      async authenticate(username, password, email) {
+          // Send the user data to the server
+          const response = await fetch('/php/login.php', {
+              method: 'POST',
+              headers: {
+                  'Content-Type': 'application/json'
+              },
+              body: JSON.stringify({ username, password, email })
+          });
+
+          const user = await response.json();
+
+          if (user) {
+              localStorage.setItem('user', JSON.stringify(user));
+              this.user = user;
+              return user;
+          }
+          return false;
+      },
+      ImageSrc(src) {
+          return 'http://127.0.0.1:5500' + src;
       }
-    });
   });
+});
